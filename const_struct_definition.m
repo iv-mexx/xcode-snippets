686d7457771fa18c404f2e46afea09c752f453fe
MeXx <mexx@devsub.net>
2014-03-18 11:32:04 +0100

    Imported user code snippets

---
+++ const_struct_definition.m
@@ -0,0 +1,12 @@
+// Const Struct Definition
+// 
+//
+// IDECodeSnippetCompletionPrefix: const struct
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 4AD9F440-8D4D-4C21-A04A-23CE684F1B49
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+const struct <#IVConstant#> <#IVConstant#> = {
+    .<#constant#> = @"<#constant#>",
+};
---
+++ ddlogimport.m
@@ -0,0 +1,15 @@
+// DDLogImport
+// 
+//
+// IDECodeSnippetCompletionPrefix: ddlogimport
+// IDECodeSnippetCompletionScopes: [TopLevel]
+// IDECodeSnippetIdentifier: 3BA557CF-39A9-48FE-A266-07A5144DAEB5
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#import "DDLog.h"
+#ifdef DEBUG
+static const int                         ddLogLevel = LOG_LEVEL_WARN;
+#else
+static const int                         ddLogLevel = LOG_LEVEL_INFO;
+#endif
